ade48f663e37d198958b394734ec988e31b67910
RudRecciah <me@rudrecciah.dev>
2021-07-29 17:30:54 -0400

    some more prep

--- src/Main.js
+++ src/Main.js
@@ -11,8 +11,48 @@ module.exports = class Main {
     client.once("ready", () => {
       console.log("We have logged in.");
       console.log("Node.js Application Loaded");
+      this.cli();
     });
     await client.login(this.Config.token);
+  }
+
+  static cli() {
+
+    const readline = require("readline");
+
+    const questions = [
+      ["Can the fox be recursive?", "O_o"],
+      ["Can readline be non-recursive?", "Yep"]
+    ];
+
+    const cmd = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+
+    function askQuestion() {
+      if(!questions.length) {
+        return cmd.close();
+      }
+      cmd.output.write(`${questions[0][0]} `);
+      cmd.resume();
+    }
+
+    function checkAnswer(answer) {
+      if(answer === questions[0][1]) {
+        cmd.output.write("Correct!\n");
+      }else{
+        cmd.output.write("Wrong!\n");
+      }
+      questions.shift();
+      cmd.pause();
+    }
+
+    cmd.on("pause", askQuestion);
+
+    cmd.on("line", checkAnswer);
+
+    askQuestion();
   }
 
 };
